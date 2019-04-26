8245a36e0f6bf5b901e5e73935cda4f242da0596
Yongteng Huang <charliehuang1017@gmail.com>
2019-04-26 11:24:02 +0800

    Kotlin variable, data type

--- src/main/kotlin/com/yt/Hello.kt
+++ src/main/kotlin/com/yt/Hello.kt
@@ -5,9 +5,25 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+
+    var a: Double
+    var b: Float
+    var c: Int
+    var d: Short
+    var e: Long
+    var f: Boolean
+    var g: Byte
+    var i: Char
+    var j: String
+
 }
 
 class Human {
+    var a: Double = 0.0
+    var b: Float = 0.0f
+    val c: Int = 1
+
+
     fun hello() {
         println("Hello Kotlin");
     }
